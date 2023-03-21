3d14529616b37d0e3ba7bf96ea09756d2439b50f
DanDiazRivera <ddiazrivera101@gmail.com>
2023-03-21 17:10:09 -0400

    Final Commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform startMarker;
+public Transform endMarker;
+
+// Movement speed in units/sec.
+public float speed = 1.0f;
+
+// Time when the movement started.
+private float startTime;
+
+// Total distance between the markers.
+private float journeyLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Distance moved = time * speed.
+          float distCovered = (Time.time - startTime) * speed;
+
+     // Fraction of journey completed = current distance divided by total distance.
+          float fracJourney = distCovered / journeyLength;
+
+     // Set our position as a fraction of the distance between the markers and pingpong the movement.
+          transform.position = Vector3.Lerp(startMarker.position, endMarker.position, Mathf.PingPong (fracJourney, 1));
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,18 +6,32 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
     public float speed;
-
     public Text score;
-
+    public Text lives;
     private int scoreValue = 0;
+    private int livesValue = 3;
+    private bool tpFlag = true;
+    private bool facingRight = true;
+    public AudioClip winSound;
+    public AudioClip loseSound;
+    public AudioClip backgroundMusic;
+    public AudioSource musicSource;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Coins: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
+        anim = GetComponent<Animator>();
+        musicSource.clip = backgroundMusic;
+        musicSource.Play();
     }
 
     // Update is called once per frame
@@ -27,18 +41,76 @@ public class PlayerScript : MonoBehaviour
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
     }
-
+    void Update()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        if (vertMovement != 0)
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+     if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+     if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+     if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+     if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        } else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+      if (hozMovement == 0 && vertMovement == 0){
+            anim.SetInteger("State", 0);
+      }
+    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Coins: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if(scoreValue >= 8){
+                winTextObject.SetActive(true);
+                Destroy(gameObject);
+                musicSource.clip = winSound;
+                musicSource.Play();
+            }
+            if (scoreValue == 4 && tpFlag == true) {
+		        transform.position = new Vector3(59f, -2f, 0.0f);
+		        tpFlag = false;
+                livesValue = 3;
+                lives.text = "Lives: " + livesValue.ToString();
+		    }
         }
-
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if(livesValue <= 0){
+                loseTextObject.SetActive(true);
+                Destroy(gameObject);
+                musicSource.clip = loseSound;
+                musicSource.Play();
+            }
+        }
     }
-
+    
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ground")
@@ -49,4 +121,11 @@ public class PlayerScript : MonoBehaviour
             }
         }
     }
+    void Flip()
+   {
+     facingRight = !facingRight;
+     Vector2 Scaler = transform.localScale;
+     Scaler.x = Scaler.x * -1;
+     transform.localScale = Scaler;
+   }
 }
